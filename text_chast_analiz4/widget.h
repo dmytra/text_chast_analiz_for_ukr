d4feb79e27ec0697daad2d574520ea8d2df6fa8a
dmytra bob <105235692+dmytra@users.noreply.github.com>
2023-06-24 01:26:35 +0300

    версія 4

---
+++ text_chast_analiz4/widget.h
@@ -0,0 +1,46 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+class QPushButton;
+class QTextEdit;
+class QLineSeries;
+class QChart;
+class QChartView;
+class QLabel;
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    int alphabet[33] = {0};
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    QTextEdit   *textEdit;
+    QPushButton *insertTextObjectButton;
+    QPushButton *insertFileObjectButton;
+    QPushButton *insertSaveObjectButton;
+    QLineSeries *series;
+    QChart      *chart;
+    QChartView  *chartView;
+    QLabel * lblStatLabel;
+
+private slots:
+    void insertTextObject();
+    void insertFileObject();
+    void insertStatLabel();
+    void saveStat();
+
+private:
+    void insertSeries();
+    void addTextDB();
+    void addFileDB();
+    void caseLetters(QString);
+};
+#endif // WIDGET_H
